64b32aa275658cea92b58623c30f698172d03824
HemachandranS <85756930+HemachandranS@users.noreply.github.com>
2023-02-18 16:16:33 +0530

    Create working.cpp

---
+++ working.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, world!" << endl;
+    cout << "The sum of 2 and 3 is: " << 2 + 3 << endl;
+    cout << "This statement will be printed in " << 3 << " seconds." << endl;
+    cout << "The value of pi is approximately " << 3.14159 << endl;
+    cout << "This is the last print statement." << endl;
+
+    return 0;
+}
