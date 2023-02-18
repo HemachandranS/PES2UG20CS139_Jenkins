8c8fb5c4200a907c1df2f918e2d49653417c25de
HemachandranS <85756930+HemachandranS@users.noreply.github.com>
2023-02-18 12:57:53 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "webhooks\n";
+  cout << "This is after the changes\n";
   return 0;
 }
